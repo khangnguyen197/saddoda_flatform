9a48241930e01ace51a7d8bfa088f99c8f09a39e
Huu-Khang Nguyen <65224957+khangnguyen197@users.noreply.github.com>
2021-01-04 20:08:10 +0700

    backup before add filter

--- app/src/main/java/com/example/finalproject/FilterActivity.java
+++ app/src/main/java/com/example/finalproject/FilterActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
@@ -19,14 +21,26 @@ public class FilterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_filter);
         globals.transStatus(getWindow());
         setImage();
+        setClick();
     }
 
     /**Load copied image from another activities */
     private void setImage(){
         ImageView showImg = (ImageView) findViewById(R.id.showImg);
         showImg.setImageDrawable(new BitmapDrawable(getResources(), WelcomeActivity.getBitmap_transfer()));
+    }
 
+    /**Click event */
+    private void setClick(){
+        Button backBtn = (Button) findViewById(R.id.back_button);
 
-
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FilterActivity.this, WelcomeActivity.class);
+               // setContentView(R.layout.edit_option);
+                startActivity(intent);
+            }
+        });
     }
 }
